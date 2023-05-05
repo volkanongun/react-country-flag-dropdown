8050b77bcbc2898790d250e8a9e2c9df8a106496
Volkan Ongun <volkan.ongun@afiniti.com>
2023-05-05 18:14:50 +0300

    dryrun successful with component

--- dist/esm/components/PhoneNumberSelectorWithFlags/CountryDropdown.d.ts
+++ dist/esm/components/PhoneNumberSelectorWithFlags/CountryDropdown.d.ts
@@ -1,4 +1,4 @@
 import React from 'react';
-import { CountryDropdownProps } from '../types';
+import { CountryDropdownProps } from './types';
 declare const CountryDropdown: React.FC<CountryDropdownProps>;
 export default CountryDropdown;
--- dist/esm/components/PhoneNumberSelectorWithFlags/CountryListItem.d.ts
+++ dist/esm/components/PhoneNumberSelectorWithFlags/CountryListItem.d.ts
@@ -1,4 +1,4 @@
 import React from 'react';
-import { CountryListItemProps } from '../types';
+import { CountryListItemProps } from './types';
 declare const CountryListItem: React.FC<CountryListItemProps>;
 export default CountryListItem;
